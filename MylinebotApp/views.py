b7984f394f88610fae3ce39312bb1d8b6b3baa84
Tiffany Fan <tiffany.mg10@nycu.edu.tw>
2022-09-14 00:49:37 +0800

    first version

--- MylinebotApp/views.py
+++ MylinebotApp/views.py
@@ -31,84 +31,79 @@ def callback(request):
             return HttpResponseBadRequest()
 
         for event in events:
-            # if isinstance(event, MessageEvent):
-            #     mtext=event.message.text
-            #     uid=event.source.user_id
-            #     profile=line_bot_api.get_profile(uid)
-            #     name=profile.display_name
-            #     pic_url=profile.picture_url
-            #     points=0
-
-            #     message=[]
-            #     if event.message.text=='開始遊戲':
-            #         if User_Info.objects.filter(uid=uid).exists()==False:
-            #             User_Info.objects.create(uid=uid,name=name,pic_url=pic_url,mtext=mtext,points=points)
-            #             message.append(TextSendMessage(text='註冊成功'))
-            #         else:
-            #             message.append(TextSendMessage(text='已註冊'))
-            #     elif event.message.text=='查看黑客松幣總額':
-            #         user_info = User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url)
-            #         for user in user_info:
-            #             info = 'points=%s'%(user.points)
-            #             message.append(TextSendMessage(text=info))
-            #     elif event.message.text=='各企業任務總覽':
-            #         message.append(TextSendMessage(text='以下是各企業任務總覽\n台積電\nASML\nKronos\nNXP\n意法'))
-            #     elif event.message.text=='活動規則':
-            #         message.append(TextSendMessage(text='以下是活動規則\n現在輸入「增加點數」就可以增加100~300點\n試試看'))
-            #     elif event.message.text=='確定兌換 Level 1 抽獎卷':
-            #         user_info = User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url) 
-            #         new_points=0
-            #         less_point=1000
-            #         for user in user_info:
-            #             new_points = user.points - less_point
-            #         User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url).update(points=new_points) #修改
-            #         for user in user_info:
-            #             info = 'points=%s'%(new_points)
-            #             message.append(TextSendMessage(text=info)) #輸出
-            #         if new_points < 0 :
-            #             message.append(TextSendMessage(text='喔不你的錢錢不夠，到了抽獎區也不能抽獎喔'))
-            #         else :
-            #             message.append(TextSendMessage(text='請到抽獎區抽獎，並出示黑客松幣餘額'))
-            #     elif event.message.text=='確定兌換 Level 2 抽獎卷':
-            #         user_info = User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url) 
-            #         new_points=0
-            #         less_point=2000
-            #         for user in user_info:
-            #             new_points = user.points - less_point
-            #         User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url).update(points=new_points) #修改
-            #         for user in user_info:
-            #             info = 'points=%s'%(new_points)
-            #             message.append(TextSendMessage(text=info)) #輸出
-            #         if new_points < 0 :
-            #             message.append(TextSendMessage(text='喔不你的錢錢不夠，到了抽獎區也不能抽獎喔'))
-            #         else :
-            #             message.append(TextSendMessage(text='請到抽獎區抽獎，並出示黑客松幣餘額'))
-            #     elif event.message.text=='輸出會員資料':
-            #         user_info = User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url)
-            #         for user in user_info:
-            #             info = 'UID=%s\nNAME=%s\n大頭貼=%s\npoints=%s\n'%(user.uid,user.name,user.pic_url,user.points)
-            #             message.append(TextSendMessage(text=info))
-            #     elif event.message.text=='增加點數':
-            #         user_info = User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url) 
-            #         new_points=0
-            #         add_point=random.randrange(100,301,100)
-            #         for user in user_info:
-            #             new_points = user.points + add_point
-            #         User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url).update(points=new_points) #修改
-            #         for user in user_info:
-            #             info = 'points=%s'%(new_points)
-            #             message.append(TextSendMessage(text=info))
-            #     elif event.message.text=='刪除會員資料':
-            #         User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url).delete() #刪除
-            #     #else:
-            #         #message.append(TextSendMessage(text='再想想'))
-            #     line_bot_api.reply_message(event.reply_token,message)
-
-            #     return HttpResponse()
-            # else:
-            #     return HttpResponseBadRequest()
             if isinstance(event, MessageEvent):
                 mtext=event.message.text
+                uid=event.source.user_id
+                profile=line_bot_api.get_profile(uid)
+                name=profile.display_name
+                pic_url=profile.picture_url
+                points=0
+
                 message=[]
-                message.append(TextSendMessage(text=mtext))
+                if event.message.text=='開始遊戲':
+                    if User_Info.objects.filter(uid=uid).exists()==False:
+                        User_Info.objects.create(uid=uid,name=name,pic_url=pic_url,mtext=mtext,points=points)
+                        message.append(TextSendMessage(text='註冊成功'))
+                    else:
+                        message.append(TextSendMessage(text='已註冊'))
+                elif event.message.text=='查看黑客松幣總額':
+                    user_info = User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url)
+                    for user in user_info:
+                        info = 'points=%s'%(user.points)
+                        message.append(TextSendMessage(text=info))
+                elif event.message.text=='各企業任務總覽':
+                    message.append(TextSendMessage(text='以下是各企業任務總覽\n台積電\nASML\nKronos\nNXP\n意法'))
+                elif event.message.text=='活動規則':
+                    message.append(TextSendMessage(text='以下是活動規則\n現在輸入「增加點數」就可以增加100~300點\n試試看'))
+                elif event.message.text=='確定兌換 Level 1 抽獎卷':
+                    user_info = User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url) 
+                    new_points=0
+                    less_point=1000
+                    for user in user_info:
+                        new_points = user.points - less_point
+                    User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url).update(points=new_points) #修改
+                    for user in user_info:
+                        info = 'points=%s'%(new_points)
+                        message.append(TextSendMessage(text=info)) #輸出
+                    if new_points < 0 :
+                        message.append(TextSendMessage(text='喔不你的錢錢不夠，到了抽獎區也不能抽獎喔'))
+                    else :
+                        message.append(TextSendMessage(text='請到抽獎區抽獎，並出示黑客松幣餘額'))
+                elif event.message.text=='確定兌換 Level 2 抽獎卷':
+                    user_info = User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url) 
+                    new_points=0
+                    less_point=2000
+                    for user in user_info:
+                        new_points = user.points - less_point
+                    User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url).update(points=new_points) #修改
+                    for user in user_info:
+                        info = 'points=%s'%(new_points)
+                        message.append(TextSendMessage(text=info)) #輸出
+                    if new_points < 0 :
+                        message.append(TextSendMessage(text='喔不你的錢錢不夠，到了抽獎區也不能抽獎喔'))
+                    else :
+                        message.append(TextSendMessage(text='請到抽獎區抽獎，並出示黑客松幣餘額'))
+                elif event.message.text=='輸出會員資料':
+                    user_info = User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url)
+                    for user in user_info:
+                        info = 'UID=%s\nNAME=%s\n大頭貼=%s\npoints=%s\n'%(user.uid,user.name,user.pic_url,user.points)
+                        message.append(TextSendMessage(text=info))
+                elif event.message.text=='增加點數':
+                    user_info = User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url) 
+                    new_points=0
+                    add_point=random.randrange(100,301,100)
+                    for user in user_info:
+                        new_points = user.points + add_point
+                    User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url).update(points=new_points) #修改
+                    for user in user_info:
+                        info = 'points=%s'%(new_points)
+                        message.append(TextSendMessage(text=info))
+                elif event.message.text=='刪除會員資料':
+                    User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url).delete() #刪除
+                #else:
+                    #message.append(TextSendMessage(text='再想想'))
                 line_bot_api.reply_message(event.reply_token,message)
+
+                return HttpResponse()
+            else:
+                return HttpResponseBadRequest()
