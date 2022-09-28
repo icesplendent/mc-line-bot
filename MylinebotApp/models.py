974e5d188d00a665465b5b9ec8678ce2ccb56cbe
Tiffany Fan <tiffany.mg10@nycu.edu.tw>
2022-09-28 17:28:52 +0800

    first version2

--- MylinebotApp/models.py
+++ MylinebotApp/models.py
@@ -8,6 +8,18 @@ class User_Info(models.Model):
     mtext = models.CharField(max_length=255,blank=True,null=False)      #文字訊息紀錄
     mdt = models.DateTimeField(auto_now=True)                           #物件儲存的日期時間
     points = models.IntegerField(default=0)                             #點數
+    STM = models.IntegerField(default=0)                                #意法
+    Yahoo = models.IntegerField(default=0)
+    ASML = models.IntegerField(default=0)
+    NXP = models.IntegerField(default=0)
+    Chunghwa = models.IntegerField(default=0)                           #中華電信
+    Kronos = models.IntegerField(default=0)
+    Cathay = models.IntegerField(default=0)                             #國泰
+    CTBC = models.IntegerField(default=0)                               #中信金控
+    Line = models.IntegerField(default=0)
+    tsmc = models.IntegerField(default=0)
+    a104 = models.IntegerField(default=0)
+    Pixart = models.IntegerField(default=0)
 
     def __str__(self):
         return self.uid
