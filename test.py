1515aa78615aa29a4b4dd26db66944c0b4935771
Tiffany Fan <tiffany.mg10@nycu.edu.tw>
2022-10-06 19:07:10 +0800

    1006 version

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+'''
 import random
 
 times=1000
@@ -45,4 +46,29 @@ print("2400=")
 print(up2400)
 print("1900=")
 print(up1900)
+'''
+def anyToDecimal(num,n):
+    baseStr = {"0":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,
+               "A":10,"B":11,"C":12,"D":13,"E":14,"F":15,"G":16,"H":17,"I":18,"J":19,
+               "K":20,"L":21,"M":22,"N":23,"O":24,"P":25,"Q":26,"R":27,"S":28,"T":29,
+               "U":30,"V":31,"W":32,"X":33,"Y":34,"Z":35}
+    new_num = 0
+    nNum = len(num) - 1
+    for i in num:         
+        new_num = new_num  + baseStr[i]*pow(n,nNum)
+        nNum = nNum -1 
+    return new_num
+def secretnum(a):
+    #a = input()
+    anyToDecimal(a,36)
+    print(anyToDecimal(a,36) % 2022)
+    return anyToDecimal(a,36) % 2022 #這裡可以拿到最後要的mod出來的值 2022要記得改掉
+
+
+bb=input()
+x = bb.split(":")
+print(x[1])
+seaftermod = secretnum(x[1])
+info = 'aftermod=%s'%(seaftermod)
+print(info)
 
