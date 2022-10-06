25f7cebe7055fd31054a8aced4c232d3e5f9d417
Tiffany Fan <tiffany.mg10@nycu.edu.tw>
2022-10-06 17:53:30 +0800

    1006 version

--- MylinebotApp/views.py
+++ MylinebotApp/views.py
@@ -18,6 +18,23 @@ line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
 
+#解密碼函式
+def anyToDecimal(num,n):
+    baseStr = {"0":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,
+               "A":10,"B":11,"C":12,"D":13,"E":14,"F":15,"G":16,"H":17,"I":18,"J":19,
+               "K":20,"L":21,"M":22,"N":23,"O":24,"P":25,"Q":26,"R":27,"S":28,"T":29,
+               "U":30,"V":31,"W":32,"X":33,"Y":34,"Z":35}
+    new_num = 0
+    nNum = len(num) - 1
+    for i in num:         
+        new_num = new_num  + baseStr[i]*pow(n,nNum)
+        nNum = nNum -1 
+    return new_num
+def secretnum(a)
+    a = input()
+    anyToDecimal(a,36)
+    return anyToDecimal(a,36) % 2022 #這裡可以拿到最後要的mod出來的值 2022要記得改掉
+
 @csrf_exempt
 def callback(request):
     if request.method == 'POST':
@@ -113,7 +130,12 @@ def callback(request):
                 elif event.message.text=='刪除會員資料':
                     User_Info.objects.filter(uid=uid,name=name,pic_url=pic_url).delete() #刪除
                 elif ('Line:'in event.message.text):
-                     message.append(TextSendMessage(text='他要輸入line的密碼了'))
+                    message.append(TextSendMessage(text='他要輸入line的密碼了'))
+                    x = event.message.text.split(":")
+                    seaftermod = secretnum(x[1])
+                    info = 'aftermod=%s'%(secretnum)
+                    message.append(TextSendMessage(text=info))
+
                 else:
                     message.append(TextSendMessage(text='再想想'))
                 line_bot_api.reply_message(event.reply_token,message)
